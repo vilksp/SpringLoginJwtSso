a2ff297f237d16bca883771a563fa1d23b20ca29
vilksp <v.kasperavicius1@gmail.com>
2021-01-30 20:43:30 +0200

    UserService interface with base method register user added

--- src/main/java/ksp/vilius/services/UserServiceI.java
+++ src/main/java/ksp/vilius/services/UserServiceI.java
@@ -1,5 +1,9 @@
 package ksp.vilius.services;
 
+import ksp.vilius.dto.CreateUserDto;
+import ksp.vilius.models.ApplicationUser;
+
 public interface UserServiceI {
 
+    public ApplicationUser registerNewUser(CreateUserDto createUserDto);
 }
---
+++ src/main/java/ksp/vilius/services/implementation/UserServiceImpl.java
@@ -0,0 +1,2 @@
+package ksp.vilius.services.implementation;public class UserServiceImpl {
+}
