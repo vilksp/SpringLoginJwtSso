9d701948d6b8d97f6d2338f223cb1eb64534f983
vilksp <v.kasperavicius1@gmail.com>
2021-01-30 20:26:51 +0200

    Id added to user model

--- src/main/java/ksp/vilius/models/ApplicationUser.java
+++ src/main/java/ksp/vilius/models/ApplicationUser.java
@@ -5,11 +5,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
 @AllArgsConstructor
 public class ApplicationUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String username;
     private String email;
     private String firstName;
