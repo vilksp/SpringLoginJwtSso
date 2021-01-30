28e66ab0994d7da4805f142bc4156eb219a237b8
vilksp <v.kasperavicius1@gmail.com>
2021-01-30 22:58:17 +0200

    endpoint to register user added

--- src/main/java/ksp/vilius/controllers/ApplicationUserController.java
+++ src/main/java/ksp/vilius/controllers/ApplicationUserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/user")
 public class ApplicationUserController {
+
     @Autowired
     private UserServiceI userService;
 
