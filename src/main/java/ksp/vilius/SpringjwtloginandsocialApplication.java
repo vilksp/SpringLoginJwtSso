6fce9a141f1589dd3e5536fb355f22d08c1f45d5
vilksp <v.kasperavicius1@gmail.com>
2021-01-30 19:39:12 +0200

    Initial commit

---
+++ src/main/java/ksp/vilius/SpringjwtloginandsocialApplication.java
@@ -0,0 +1,13 @@
+package ksp.vilius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjwtloginandsocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjwtloginandsocialApplication.class, args);
+	}
+
+}
