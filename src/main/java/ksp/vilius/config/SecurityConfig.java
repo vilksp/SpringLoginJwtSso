f8fca96d9310693d40e62068fcb014796db7a94a
vilksp <v.kasperavicius1@gmail.com>
2021-01-31 21:04:57 +0200

    Added to security config exception handling auth entrypoint

--- src/main/java/ksp/vilius/config/SecurityConfig.java
+++ src/main/java/ksp/vilius/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package ksp.vilius.config;
 
+import ksp.vilius.security.JwtAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,9 +17,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 )
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private JwtAuthenticationEntryPoint authenticationEntryPoint;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
+                .and()
                 .authorizeRequests()
                 .antMatchers("/api/**").permitAll()
                 .and()
