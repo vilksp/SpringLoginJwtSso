5c414c9ff182d772f3594a0356b7afebf719d640
vilksp <v.kasperavicius1@gmail.com>
2021-01-31 21:03:18 +0200

    Class for 401 requests added

---
+++ src/main/java/ksp/vilius/payload/InvalidLoginResponse.java
@@ -0,0 +1,15 @@
+package ksp.vilius.payload;
+
+import lombok.Data;
+
+@Data
+public class InvalidLoginResponse {
+
+    private String username;
+    private String password;
+
+    public InvalidLoginResponse() {
+        this.username = "Invalid username";
+        this.password = "Invalid password";
+    }
+}
