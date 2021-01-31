8ec189e66e9eb64ea01f983e84f03ec69c5f6fde
vilksp <v.kasperavicius1@gmail.com>
2021-01-31 16:02:25 +0200

    while creating user isEnabled,isActive fields are set to true

--- src/main/java/ksp/vilius/services/implementation/UserServiceImpl.java
+++ src/main/java/ksp/vilius/services/implementation/UserServiceImpl.java
@@ -47,7 +47,7 @@ public class UserServiceImpl implements UserServiceI {
         // everytime user is created it will get by Default USER role and authorities
         userToCreate.setRole(ROLE_USER);
         //Role class haves all authorities from AuthorityConstant and now we are getting string array of all authorities from our ROLE_USER and setting it
-        userToCreate.setAuthorities(userToCreate.getRole().getAuthorities());
+        userToCreate.setAuthority(userToCreate.getRole().getAuthorities());
         log.info("new user is being created with username: " + userDto.getUsername());
         return userRepository.save(userToCreate);
 
