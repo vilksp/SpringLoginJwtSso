f562a52dbf9f4d286a8eb4e1d15021ed06f8799b
vilksp <v.kasperavicius1@gmail.com>
2021-01-31 16:02:00 +0200

    User class that implements UserDetails contract is added

--- src/main/java/ksp/vilius/security/SecurityUser.java
+++ src/main/java/ksp/vilius/security/SecurityUser.java
@@ -1,2 +1,54 @@
-package ksp.vilius.security;public class SecurityUser {
+package ksp.vilius.security;
+
+import ksp.vilius.models.ApplicationUser;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class SecurityUser extends ApplicationUser implements UserDetails {
+
+    public SecurityUser(final ApplicationUser user) {
+        super(user);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.stream(getAuthority())
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return super.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return super.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return super.isLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return super.isLocked();
+    }
 }
