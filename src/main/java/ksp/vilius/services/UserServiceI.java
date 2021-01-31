37bf3aeb3d830954d2eb084e5896fa8654ca85ce
vilksp <v.kasperavicius1@gmail.com>
2021-01-31 14:15:47 +0200

    Authenticate user method added to interface

--- src/main/java/ksp/vilius/services/UserServiceI.java
+++ src/main/java/ksp/vilius/services/UserServiceI.java
@@ -2,8 +2,11 @@ package ksp.vilius.services;
 
 import ksp.vilius.dto.CreateUserDto;
 import ksp.vilius.models.ApplicationUser;
+import ksp.vilius.payload.JwtSuccessLoginResponse;
 
 public interface UserServiceI {
 
     public ApplicationUser registerNewUser(CreateUserDto createUserDto);
+
+    public JwtSuccessLoginResponse authenticateUser(LoginRequest request);
 }
