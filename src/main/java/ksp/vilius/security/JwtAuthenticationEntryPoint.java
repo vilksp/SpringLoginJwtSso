9209bb267764993228217174dacc7c102f05560d
vilksp <v.kasperavicius1@gmail.com>
2021-01-31 21:03:55 +0200

    Return error message to Json with 401 response status

--- src/main/java/ksp/vilius/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/ksp/vilius/security/JwtAuthenticationEntryPoint.java
@@ -1,2 +1,27 @@
-package ksp.vilius.security;public class JwtAuthenticationEntryPoint {
+package ksp.vilius.security;
+
+import com.google.gson.Gson;
+import ksp.vilius.payload.InvalidLoginResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e)
+            throws IOException, ServletException {
+
+        InvalidLoginResponse invalidLoginResponse = new InvalidLoginResponse();
+        String invalidLoginResponseToJson = new Gson().toJson(invalidLoginResponse);
+        response.setContentType("application/json");
+        response.setStatus(401);
+        response.getWriter().println(invalidLoginResponseToJson);
+
+    }
 }
