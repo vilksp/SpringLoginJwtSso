e0fce0e0809f2caad93a7ba13146bbfcdb1824aa
vilksp <v.kasperavicius1@gmail.com>
2021-01-31 16:01:19 +0200

    added additional fields to user isLocked, isEnabled

--- src/main/java/ksp/vilius/models/ApplicationUser.java
+++ src/main/java/ksp/vilius/models/ApplicationUser.java
@@ -15,13 +15,27 @@ public class ApplicationUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true,updatable = false)
+    @Column(unique = true, updatable = false)
     private String username;
-    @Column(unique = true,updatable = false)
+    @Column(unique = true, updatable = false)
     private String email;
     private String firstName;
     private String lastName;
     private String password;
     private Role role;
-    private String[] authorities;
+    private String[] authority;
+    private boolean isLocked;
+    private boolean isEnabled;
+
+    public ApplicationUser(ApplicationUser user) {
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.password = user.getPassword();
+        this.role = user.getRole();
+        this.authority = user.getAuthority();
+        this.isLocked = user.isLocked();
+        this.isEnabled = user.isEnabled();
+    }
 }
