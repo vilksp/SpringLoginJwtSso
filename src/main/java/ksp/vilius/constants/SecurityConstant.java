344397102debc1318e6dbc22c71a983e6b6355d3
vilksp <v.kasperavicius1@gmail.com>
2021-02-01 17:21:03 +0200

    added constant class to clean up code in JwtTokenProvider Class

---
+++ src/main/java/ksp/vilius/constants/SecurityConstant.java
@@ -0,0 +1,8 @@
+package ksp.vilius.constants;
+
+public class SecurityConstant {
+
+    public static final long EXPIRATION_TIME = 60 * 60 * 24 * 7 * 1000;
+    public static final String JWT_TOKEN_ISSUER = "vilksp";
+    public static final String JWT_TOKEN_SECRET = "3207C63AFF1E40703056B951343BD22C16851D3ED2D5DA257AB094E71BBD1712";
+}
--- src/main/java/ksp/vilius/security/JwtTokenProvider.java
+++ src/main/java/ksp/vilius/security/JwtTokenProvider.java
@@ -8,12 +8,11 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import static ksp.vilius.constants.SecurityConstant.*;
+
 @Component
 public class JwtTokenProvider {
 
-    public static final long EXPIRATION_TIME = 60 * 60 * 24 * 7 * 1000;
-    public static final String JWT_TOKEN_ISSUER = "vilksp";
-    public static final String JWT_TOKEN_SECRET = "3207C63AFF1E40703056B951343BD22C16851D3ED2D5DA257AB094E71BBD1712";
 
     public String generateToken(Authentication auth) {
 
