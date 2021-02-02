a3a26647e501155a845ea8d3043a7183a8f22a6e
vilksp <v.kasperavicius1@gmail.com>
2021-02-02 15:38:10 +0200

    Service that implements OAuth2Service contract added

--- src/main/java/ksp/vilius/security/oauth2/CustomOAuth2Service.java
+++ src/main/java/ksp/vilius/security/oauth2/CustomOAuth2Service.java
@@ -1,4 +1,15 @@
 package ksp.vilius.security.oauth2;
 
-public class CustomOAuth2Service {
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOAuth2Service implements OAuth2UserService {
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest) throws OAuth2AuthenticationException {
+        return null;
+    }
 }
