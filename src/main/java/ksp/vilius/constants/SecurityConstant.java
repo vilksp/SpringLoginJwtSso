c0abe215d833c970f01402f6539c977ab87406b1
vilksp <v.kasperavicius1@gmail.com>
2021-02-03 18:57:50 +0200

    oauth authorization path added to be able to access without auth

--- src/main/java/ksp/vilius/constants/SecurityConstant.java
+++ src/main/java/ksp/vilius/constants/SecurityConstant.java
@@ -5,5 +5,5 @@ public class SecurityConstant {
     public static final long EXPIRATION_TIME = 60 * 60 * 24 * 7 * 1000;
     public static final String JWT_TOKEN_ISSUER = "vilksp";
     public static final String JWT_TOKEN_SECRET = "3207C63AFF1E40703056B951343BD22C16851D3ED2D5DA257AB094E71BBD1712";
-    public static final String[] ALLOWED_URLS_WITH_NO_AUTH = {"/api/user/login","/api/user/register"};
+    public static final String[] ALLOWED_URLS_WITH_NO_AUTH = {"/api/user/login", "/api/user/register", "/oauth2/authorization/github"};
 }
---
+++ src/main/java/ksp/vilius/events/SuccessfulAuthenticationHandler.java
@@ -0,0 +1,2 @@
+package ksp.vilius.events;public class SuccessfulAuthenticationHandler {
+}
