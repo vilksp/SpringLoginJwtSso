56285647904a7f60e0d00b12ba02a4e3bbe642a6
vilksp <v.kasperavicius1@gmail.com>
2021-02-03 21:09:30 +0200

    update on successhandler

--- src/main/java/ksp/vilius/events/SuccessfulAuthenticationHandler.java
+++ src/main/java/ksp/vilius/events/SuccessfulAuthenticationHandler.java
@@ -1,7 +1,10 @@
 package ksp.vilius.events;
 
+import ksp.vilius.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -13,11 +16,20 @@ import java.io.IOException;
 @Component
 public class SuccessfulAuthenticationHandler implements AuthenticationSuccessHandler {
 
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
             throws IOException, ServletException {
 
-        Object OAuth2UserPrincipals = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        //Authentication class is giving OAuth2User and we have to convert it to user that our DB understand and create JWT
+        Object oAuth2UserPrincipals = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String oAuth2UsersUsername = ((DefaultOAuth2User) oAuth2UserPrincipals).getAttributes().get("login").toString();
+
+
+
+
 
     }
 }
