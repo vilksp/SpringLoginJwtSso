574c9f4c09f04fc639905e49469c59815639ebf9
vilksp <v.kasperavicius1@gmail.com>
2021-02-03 18:59:48 +0200

    AuthenticationSuccessHandler bean created

--- src/main/java/ksp/vilius/config/BeanConfig.java
+++ src/main/java/ksp/vilius/config/BeanConfig.java
@@ -1,21 +1,28 @@
 package ksp.vilius.config;
 
+import ksp.vilius.events.SuccessfulAuthenticationHandler;
 import ksp.vilius.security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 public class BeanConfig {
 
     @Bean
-    PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    JwtFilter jwtFilter() {
+    public JwtFilter jwtFilter() {
         return new JwtFilter();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler successfulAuthenticationHandler() {
+        return new SuccessfulAuthenticationHandler();
     }
 }
