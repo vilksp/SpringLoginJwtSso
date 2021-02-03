93b9dd5d45415ac87c698badf27cfe8e54b0dc6f
vilksp <v.kasperavicius1@gmail.com>
2021-02-03 18:58:05 +0200

    AuthenticationSuccessHandler added

--- src/main/java/ksp/vilius/events/SuccessfulAuthenticationHandler.java
+++ src/main/java/ksp/vilius/events/SuccessfulAuthenticationHandler.java
@@ -1,2 +1,23 @@
-package ksp.vilius.events;public class SuccessfulAuthenticationHandler {
+package ksp.vilius.events;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class SuccessfulAuthenticationHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+            throws IOException, ServletException {
+
+        Object OAuth2UserPrincipals = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+    }
 }
