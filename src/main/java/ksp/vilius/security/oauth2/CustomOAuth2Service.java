6209dc526bb9a334de7db248e416100a3d650024
vilksp <v.kasperavicius1@gmail.com>
2021-02-03 13:25:55 +0200

    OAuth2Service that extends DefaultOAuth2Service and overrides loaduser method

--- src/main/java/ksp/vilius/security/oauth2/CustomOAuth2Service.java
+++ src/main/java/ksp/vilius/security/oauth2/CustomOAuth2Service.java
@@ -1,15 +1,18 @@
 package ksp.vilius.security.oauth2;
 
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomOAuth2Service implements OAuth2UserService {
+public class CustomOAuth2Service extends DefaultOAuth2UserService {
+
+
     @Override
-    public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest) throws OAuth2AuthenticationException {
-        return null;
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        return new CustomOauth2user(oAuth2User);
     }
 }
