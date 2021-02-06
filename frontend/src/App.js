8a12b6352356251dca95b515ed2c1459d4f17000
vilksp <v.kasperavicius1@gmail.com>
2021-02-06 14:00:45 +0200

    routes for login,register,homepage added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import Login from "./components/login-component/Login";
+import Register from "./components/register-component/Register";
 
 function App() {
 	return (
@@ -12,6 +13,9 @@ function App() {
 				</Route>
 				<Route exact path="/login">
 					<Login />
+				</Route>
+				<Route exact path="/register">
+					<Register />
 				</Route>
 			</Router>
 		</div>
