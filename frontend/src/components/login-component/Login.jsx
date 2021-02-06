29c8d5d977b7c9bdbcff35c6c8f89ca18018f717
vilksp <v.kasperavicius1@gmail.com>
2021-02-06 14:16:01 +0200

    changed from class component to functional component, added Link to registerpage

--- frontend/src/components/login-component/Login.jsx
+++ frontend/src/components/login-component/Login.jsx
@@ -1,22 +1,24 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./login.css";
-class Login extends Component {
-	render() {
-		return (
-			<div className="login-box">
-				<h1>Login</h1>
-				<div className="text-box">
-					<input type="text" placeholder="Username" value=""></input>
-				</div>
-				<div className="text-box">
-					<input type="password" placeholder="Password" value=""></input>
-				</div>
-				<input className="btn" type="button" value="Sign in" />
-				<button className="btn">Login with GitHub</button>
-				<button className="btn">Don't have an account? Register here</button>
+function Login() {
+	const [details, setDetails] = useState({ username: "", password: "" });
+	return (
+		<div className="login-box">
+			<h1>Login</h1>
+			<div className="text-box">
+				<input type="text" placeholder="Username" value=""></input>
 			</div>
-		);
-	}
+			<div className="text-box">
+				<input type="password" placeholder="Password" value=""></input>
+			</div>
+			<input className="btn" type="button" value="Sign in" />
+			<button className="btn">Login with GitHub</button>
+			<Link to="/register">
+				<button className="btn">Don't have an account? Register here</button>
+			</Link>
+		</div>
+	);
 }
 
 export default Login;
