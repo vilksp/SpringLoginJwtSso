7c740036dda409a2513129faf6cd213fcb3f9785
vilksp <v.kasperavicius1@gmail.com>
2021-02-06 14:23:46 +0200

    form works correctly now need to add auth service to login correctly

--- frontend/src/components/login-component/Login.jsx
+++ frontend/src/components/login-component/Login.jsx
@@ -3,16 +3,37 @@ import { Link } from "react-router-dom";
 import "./login.css";
 function Login() {
 	const [details, setDetails] = useState({ username: "", password: "" });
+
+	const submitHandler = (e) => {
+		if (details.username !== "" && details.password !== "") {
+			console.log(details);
+		}
+	};
 	return (
 		<div className="login-box">
 			<h1>Login</h1>
 			<div className="text-box">
-				<input type="text" placeholder="Username" value=""></input>
+				<input
+					type="username"
+					placeholder="Username"
+					onChange={(e) => setDetails({ ...details, username: e.target.value })}
+					value={details.username}
+				></input>
 			</div>
 			<div className="text-box">
-				<input type="password" placeholder="Password" value=""></input>
+				<input
+					type="password"
+					placeholder="Password"
+					onChange={(e) => setDetails({ ...details, password: e.target.value })}
+					value={details.password}
+				></input>
 			</div>
-			<input className="btn" type="button" value="Sign in" />
+			<input
+				className="btn"
+				type="button"
+				value="Sign in"
+				onClick={submitHandler}
+			/>
 			<button className="btn">Login with GitHub</button>
 			<Link to="/register">
 				<button className="btn">Don't have an account? Register here</button>
