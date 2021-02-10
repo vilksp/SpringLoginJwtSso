db25095f4ca7d252d2e9b64f991588f6b1800654
vilksp <v.kasperavicius1@gmail.com>
2021-02-10 21:33:42 +0200

    if token valid redirect from register page

--- frontend/src/components/register-component/Register.jsx
+++ frontend/src/components/register-component/Register.jsx
@@ -1,35 +1,39 @@
 import "./register.css";
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { isTokenValid } from "../services/auth-service";
 
-class Register extends Component {
-	render() {
-		return (
-			<div className="login-box">
-				<h1>Register</h1>
-				<div className="text-box">
-					<input type="text" placeholder="Username" value=""></input>
-				</div>
-				<div className="text-box">
-					<input type="text" placeholder="Email" value=""></input>
-				</div>
-				<div className="text-box">
-					<input type="text" placeholder="FirstName" value=""></input>
-				</div>
-				<div className="text-box">
-					<input type="text" placeholder="LastName" value=""></input>
-				</div>
-				<div className="text-box">
-					<input type="password" placeholder="Password" value=""></input>
-				</div>
-				<input className="btn" type="button" value="Sign in" />
-
-				<Link to="/login">
-					<button className="btn">Already have account? Login here</button>
-				</Link>
+function Register() {
+	useEffect(() => {
+		if (isTokenValid()) {
+			window.location.replace("/");
+		}
+	});
+	return (
+		<div className="login-box">
+			<h1>Register</h1>
+			<div className="text-box">
+				<input type="text" placeholder="Username" value=""></input>
 			</div>
-		);
-	}
+			<div className="text-box">
+				<input type="text" placeholder="Email" value=""></input>
+			</div>
+			<div className="text-box">
+				<input type="text" placeholder="FirstName" value=""></input>
+			</div>
+			<div className="text-box">
+				<input type="text" placeholder="LastName" value=""></input>
+			</div>
+			<div className="text-box">
+				<input type="password" placeholder="Password" value=""></input>
+			</div>
+			<input className="btn" type="button" value="Sign in" />
+
+			<Link to="/login">
+				<button className="btn">Already have account? Login here</button>
+			</Link>
+		</div>
+	);
 }
 
 export default Register;
