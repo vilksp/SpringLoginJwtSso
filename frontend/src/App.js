e5b2f1c9235948ac939258a3e1706bb246d2803e
vilksp <v.kasperavicius1@gmail.com>
2021-03-09 15:52:38 +0200

    typo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import Login from "./components/login-component/Login";
 import Register from "./components/register-component/Register";
+import Header from "./components/header/header";
 
 function App() {
 	return (
 		<div className="App">
 			<Router>
+				<Header />
 				<Route exact path="/">
 					<Homepage />
 				</Route>
